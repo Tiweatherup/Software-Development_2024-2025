4b8d89cd9df0c18db9975594093948247590f5b3
Tiweatherup <tiweatherup@hotmail.com>
2024-12-01 09:55:02 +0100

    Commit 2-two instances

--- UserInputValidator.py
+++ UserInputValidator.py
@@ -1,13 +1,28 @@
 class UserInputValidator:
+  
+
     def validate_positive_integers(self, input_list):
+        
         valid_integers = []
         for item in input_list:
+            try:
                 num = int(item)
                 if num > 0:
                     valid_integers.append(num)
-                return valid_integers
+            except ValueError:
+                pass  
+        return valid_integers
 
-validator = UserInputValidator()
-input_strings = ["4", "abcd", "0", "-1", "18", "3.14"]
-valid_numbers = validator.validate_positive_integers(input_strings)
-print(valid_numbers)
+    def display_validation_message(self, input_list):
+        
+        valid_integers = self.validate_positive_integers(input_list)
+        if valid_integers:
+            print("The list contains valid positive integers:", valid_integers)
+        else:
+            print("The list does not contain any valid positive integers.")
+
+
+
+#validator = UserInputValidator()
+#input_strings = [ "abc", "0", "-5", "3.14"]
+#validator.display_validation_message(input_strings)
